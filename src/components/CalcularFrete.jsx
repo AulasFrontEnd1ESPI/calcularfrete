bea40fb75d38b9cb5806e9dd9af84fb94686281b
Giovana <giovanacarnevali@outlook.com>
2025-09-04 11:43:58 -0300

    Iniciando a estilização

--- src/components/CalcularFrete.jsx
+++ src/components/CalcularFrete.jsx
@@ -2,8 +2,8 @@
 const CalcularFrete = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-400 p-4">
-        <div>
-            <h1>Calculadora de Frete</h1>
+        <div className= "bg-white p-8 rounded-2xl max-w-md text-center">
+            <h1 className= "text-3x1 font-bold text-blue-600 mb-6">Calculadora de Frete</h1>
         </div>
     </div>
   )
